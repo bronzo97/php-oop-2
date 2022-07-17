87b74730ec2687c91802487cd619efe46cc64242
bronzo97 <danilobronzo97@hotmail.it>
2022-07-17 17:25:03 +0200

    added try - catch

--- index.php
+++ index.php
@@ -7,6 +7,13 @@ require "classes/PaymentHandler.php";
 
 $costumer1 = new Costumer('' ,'Rossi');
 
+try {
+    $customer->register("Mario", null);
+} catch (Exception $e) {
+    echo "Impossibile registrare l'utente. Dati inseriti non validi.";
+}
+
+
 $prodotto1 = new Toys('ball', 12, 'red', 'rubber' );
 $prodotto2 = new Food('crocchette', 4.99, '05-23', ['chicken', 'meat', 'flour']);
 
